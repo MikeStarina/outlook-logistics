20f571b54105d6420b47f90dfdd0111c2825609a
Mike Starina <x@pnhd.ru>
2023-01-09 21:08:56 +0300

    base url changed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 //export const apiUrl = 'http://localhost:1337';
-export const apiUrl = 'http://95.163.236.246:1337';
+//export const apiUrl = 'http://95.163.236.246:1337';
+export const apiUrl = 'http://outlook-logistics.ru';
