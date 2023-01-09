fafc43af1618e08172968e217c7a2c770839d41c
Mike Starina <x@pnhd.ru>
2023-01-09 18:53:53 +0300

    some data fixed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-//export const apiUrl = 'http://localhost:1337';
-export const apiUrl = 'http://95.163.236.246:1337';
+export const apiUrl = 'http://localhost:1337';
+//export const apiUrl = 'http://95.163.236.246:1337';
