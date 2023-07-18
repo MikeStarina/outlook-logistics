509a645224059525924d37cd71eb92f3229688c8
Mike Starina <x@pnhd.ru>
2023-07-18 18:19:09 +0300

    seo texts added

--- src/pages/features-page/features-page.tsx
+++ src/pages/features-page/features-page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './features-page.module.css';
-import { useParams } from 'react-router';
+import { Navigate, useParams } from 'react-router';
 import { useSelector } from '../..';
 import FirstScreen from '../../components/main-page-components/first-screen/first-screen';
 import FeaturesList from '../../components/feature-page-components/options-screen/features-list';
@@ -21,8 +21,10 @@ const FeaturesPage: React.FC = () => {
 
     const filteredData = data.filter(item => item.url === id)[0];
 
-
+    
     return (
+        <>
+        {filteredData ? (
         <main className={styles.page}>
             <Helmet>
                 <title>{filteredData.metadata?.title}</title>
@@ -33,30 +35,19 @@ const FeaturesPage: React.FC = () => {
             <FeaturesList />
             <section className={styles.screen}>
 
-                <h1 className={styles.screen_title}>Автоперевозки — рассчитать стоимость</h1>
-                <p className={styles.paragraph}>
-                    Компания Outlook Logistics ежедневно организовывает автоперевозки 20 тонн грузовым транспортом,  перевозки фурой по области, России и ближайшему зарубежью - одно из ключевых направлений нашей компании, благодаря чему мы рады можем стать универсальным подрядчиком под ваши объемы.
-
-                    Будем рады предоставить предоставить лучший сервис,  обеспечивая вас круглосуточной связью с менеджером, страхование груза на любую сумму, отсутствие перегона транспорта, из-за его наличия в каждом регионе, что позволяет нам устанавливать цену ниже конкурентов.
-                    
-                    На сайте вы можете рассчитать доставку грузов 20 тонн и более, достаточно указать пункты отправления и назначения, характеристики груза
-
-                    Мы понимаем, что наши клиенты ценят время, а потому берем на себя все документальное оформление  перевозки
-                    
-                    Мы уже сотрудничали с такими компаниями как: Gazprom, Kamaz, РЖД, TATNEFT и др., и смогли завоевать их доверие, бесперебойной работой под ключ любой сложности.
-
-                    Главной целью компании является доставка в срок, а также сохранность перевозимого груза на протяжении всей транспортировки. 
-
-                    Остались вопросы — свяжитесь с нами по почте info@outlook-logistics.ru и мы проконсультируем вас.
-
+                <h1 className={styles.screen_title}>{filteredData.name}</h1>
+                {filteredData?.description && filteredData.description.map((item, index) => (
+                <p className={styles.paragraph} key={index}>
+                    {item}
                 </p>
-
+                ))}
             </section>
             <ClientsScreen />
             <FormScreen />
             <ContactsScreen />
 
-        </main>
+        </main>) : (<Navigate to='/404'/>)}
+        </>
     )
 }
 
