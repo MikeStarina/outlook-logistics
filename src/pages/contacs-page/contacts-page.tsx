58530f1606a9f97fc3328905677925adc59dfdf2
Mike Starina <x@pnhd.ru>
2023-08-12 19:49:05 +0300

    more seo pages added

--- src/pages/contacs-page/contacts-page.tsx
+++ src/pages/contacs-page/contacts-page.tsx
@@ -15,11 +15,11 @@ const ContactsPage: React.FC = () => {
         <main className={styles.page}>    
             <section className={styles.screen}>
                 <p className={styles.contacts_item}>ООО "ПЕРСПЕКТИВА"</p>
-                <p className={styles.contacts_item}>ИНН / КПП: 4706053435 / 470601001</p>
+                <p className={styles.contacts_item}>ИНН / КПП: 4706053435 / 781601001</p>
                 <p className={styles.contacts_item}>ОГРН: 1224700019114</p>
-                <p className={styles.contacts_item}>АДРЕС: 192236, РОССИЯ СЗФО, Г САНКТ-ПЕТЕРБУРГ, УЛ СОФИЙСКАЯ 14, ОФИС 915</p>
+                <p className={styles.contacts_item}>АДРЕС: 192236, РОССИЯ СЗФО, Г САНКТ-ПЕТЕРБУРГ, УЛ СОФИЙСКАЯ 14, ОФИС 903</p>
                 <p className={styles.contacts_item}>ТЕЛЕФОН: 8(800)201-00-73</p>
-                <p className={styles.contacts_item}>РЕЖИМ РАБОТЫ: ПН-ПТ, 10-18</p>
+                <p className={styles.contacts_item}>РЕЖИМ РАБОТЫ: ПН-ПТ, 9—18</p>
             </section>
             
             <ClientsScreen />
