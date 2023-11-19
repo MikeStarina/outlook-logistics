a82a31cd32ef4c04dc7a0a2839dffbfbd8313336
Mike Starina <x@pnhd.ru>
2023-11-19 13:51:40 +0300

    y.metrika script added (test)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import Head from 'next/head';
+import Script from 'next/script';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
@@ -15,6 +16,13 @@ export const viewport = {
   // interactiveWidget: 'resizes-visual',
 }
 
+export const metadata: Metadata = {
+  verification: {
+    yandex: 'df985d3636ebc81d',
+    google: 'w2HyEYhhbDmEppg3hDL1YKutFFfbcddA0wQPxRkaGaI',
+  }
+}
+
 
 
 
@@ -26,7 +34,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}</body>    
+      <Script
+        async
+        id='metrika-counter'
+        strategy="afterInteractive"
+      >
+        {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(91973713, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+      });`}
+      </Script>
     </html>
   )
 }
